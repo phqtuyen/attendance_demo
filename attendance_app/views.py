a004fa07891ee8b4e2d78012b6b5cdacf9438fb5
unknown <phqtuyen@gmail.com>
2018-03-12 15:04:26 +1100

    handle http request

--- attendance_app/views.py
+++ attendance_app/views.py
@@ -2,32 +2,53 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
-
+from .test import test_class
 question_key = 'Question'
 #to be update using database
 question = "What is 1 + 1"
 answer = "2"
-@csrf_exempt
-def index(request):
-	if request.method == "GET" :
-		print (request.GET)
-		if question_key in request.GET:
-			if request.GET.get(question_key) == answer:
-				html = loader.get_template("views/confirm.html")
-				response = HttpResponse(html.render())
-			else: 
-				context = {"question" : question}
-				html = loader.get_template("views/question.html")		
-				response = HttpResponse(html.render(context=context))
-					
-		else:	
-			html = loader.get_template("views/create.html")
-			print (type(str(html.render())) )
-			response = HttpResponse(html.render() % {'question': question})
-	elif request.method == "POST" :
-		context = {"question" : question}
-		html = loader.get_template("views/question.html")		
+
+class AppViews:
+	path = '/attendance_app'
+	def __init__(self):
+		self.question = "What is 1 + 1"
+		self.answer = "2"		
+		self.views = "views/"
+	@csrf_exempt	
+	def createForm(self,request):
+		html = loader.get_template(self.views + "create.html")
+		submitURL = request.scheme + "://" + request.get_host() + AppViews.path + '/submit' 
+		print(submitURL)
+		context = {"submitURL" : submitURL}
 		response = HttpResponse(html.render(context=context))
-	print(response.getvalue())
-	return response	
-# Create your views here.
+		print(response.getvalue())
+		return response	
+	# Create your views here.
+	@csrf_exempt
+	def submit(self,request): 
+		print (request.get_full_path())
+		submitResultURL = request.scheme  + "://" + request.get_host() + AppViews.path + '/submitResult'
+		context = {"question" : question, "submitResultURL" : submitResultURL}
+		html = loader.get_template(self.views + "question.html")		
+		response = HttpResponse(html.render(context=context))
+		return response	
+
+	@csrf_exempt	
+	def submitResult(self, request):
+		print (request.content_type)
+		print (request.POST)
+		print (request.GET)
+		context = {}
+		html = loader.get_template(self.views + "confirm.html")
+		if (request.POST.get("confirm_ans") == self.answer):
+			context['confirmResult'] = "Success!"
+		else:
+			context['confirmResult'] = "Attendance check fail, please contact the instructor."	
+		return HttpResponse(html.render(context=context))	
+		
+	def view(self,request):
+		return
+
+	def test_output(self,request):
+		ob = test_class()
+		return HttpResponse(ob.test_print())
