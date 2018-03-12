fdcd49c0bf0838a70eb6fbe529fc6f30476dc1f0
unknown <phqtuyen@gmail.com>
2018-03-12 15:03:50 +1100

    url for each view

--- attendance_app/urls.py
+++ attendance_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-
 from . import views
+from .views import *
+import socket
 
+view = AppViews()
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', view.createForm, name='create'),
+    path('submit', view.submit, name='submit'),
+    path('submitResult', view.submitResult, name='submitResult'),
+    path('view', view.view, name='view'),
 ]
