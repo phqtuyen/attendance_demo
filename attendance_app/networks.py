c8b86ef7cdd44365898e04d4605971f9d1876124
Khang Vo <vodkhang@gmail.com>
2018-03-19 21:02:22 +1100

    Update .gitignore, install requests

---
+++ attendance_app/networks.py
@@ -0,0 +1,13 @@
+import requests
+
+class RocketUsersAPI:
+	def __init__(self, url, authToken, userID):
+		self.url = url or "http://localhost:3000/"
+		self.authToken = authToken or 'pl59Z7F1S7c5MGIMi8ZtQ6d1XAtvafqwCoc1VFoyRCN'
+		self.userID = userID or 'KEPvCAsPtzniBTdYB'
+
+	def getUsers(self):
+		headers = {'X-Auth-Token' : self.authToken, 'X-User-Id' : self.userID}
+
+		r = requests.get(self.url, headers=headers)
+		return r.text
