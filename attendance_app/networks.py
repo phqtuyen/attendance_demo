abd24d9736d2b2b3a01edb9cacddb02f7896c2a0
Khang Vo <vodkhang@gmail.com>
2018-03-19 21:21:06 +1100

    Can get users list from shell code

--- attendance_app/networks.py
+++ attendance_app/networks.py
@@ -1,13 +1,12 @@
 import requests
 
 class RocketUsersAPI:
-	def __init__(self, url, authToken, userID):
-		self.url = url or "http://localhost:3000/"
-		self.authToken = authToken or 'pl59Z7F1S7c5MGIMi8ZtQ6d1XAtvafqwCoc1VFoyRCN'
-		self.userID = userID or 'KEPvCAsPtzniBTdYB'
-
-	def getUsers(self):
-		headers = {'X-Auth-Token' : self.authToken, 'X-User-Id' : self.userID}
-
-		r = requests.get(self.url, headers=headers)
-		return r.text
+    def __init__(self, url, authToken, userID):
+        self.url = url or "http://localhost:3000/api/v1/"
+        self.authToken = authToken or 'pl59Z7F1S7c5MGIMi8ZtQ6d1XAtvafqwCoc1VFoyRCN'
+        self.userID = userID or 'KEPvCAsPtzniBTdYB'
+    def getUsers(self):
+        headers = {'X-Auth-Token' : self.authToken, 'X-User-Id' : self.userID}
+        getUsersUrl = self.url + "users.list"
+        r = requests.get(getUsersUrl, headers=headers)
+        return r.text
