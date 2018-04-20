d9f72bb98f3d8d03552c36a2f02347375c4f1e55
entropi <phqtuyen@gmail.com>
2018-04-20 13:15:26 +1000

    fix many bugs, successfully send view to admin and choices to users

--- Utility/views.py
+++ Utility/views.py
@@ -15,16 +15,16 @@ class AbstractControllers:
         if RCRole.ADMIN in st:
             return RCRole.ADMIN
         else:
-            return RCRole.USER	
+            return RCRole.USER
 
-	def userProfileFromRequest(self, params):
-		temp_profile = UserProfile()\
-						.configID(params.get('username'), params.get('source'))\
-						.configName(params.get('name'))\
-						.configEmail(params.get('email')),
-									self.get_role(params.get('role'))\
-						.configCreatedOn(None)
-		return temp_profile
+    def userProfileFromRequest(self, params):
+        temp_profile = UserProfile()\
+                                        .configID(params.get('username').strip(), params.get('source').strip())\
+                                        .configName(params.get('name').strip())\
+                                        .configEmail(params.get('email').strip(),
+                                        self.get_role(params.get('role').strip()))\
+                                        .configCreatedOn(None)
+        return temp_profile
 
     def createUserProfileIfNeeded(self, params):
         temp_prof = self.userProfileFromRequest(params)
@@ -32,9 +32,12 @@ class AbstractControllers:
         return user_prof
 
     def isAdmin(self, params):
-    	username = params.get('username')
-    	source = params.get('source')
-    	admin = UserProfile.objects.hasUserWithRole(username,
-    												source,
-    												RCRole.ADMIN) 
-		
+        username = params.get('username')
+        source = params.get('source')
+        print('check is admin:')
+        print('username: ', username, 'source: ', source)
+        admin = UserProfile.objects.hasUserWithRole(username,
+                                                                                                source,
+                                                                                                RCRole.ADMIN)
+        print('All users: ',UserProfile.objects.all())
+        return admin
