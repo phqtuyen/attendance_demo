06531b622303b74a22313174cda307c9a94f92b4
entropi <phqtuyen@gmail.com>
2018-04-23 17:30:06 +1000

    minor fixes

--- feedback/urls.py
+++ feedback/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
 	path('confirm_create_feedback', api_view.confirm_create_feedback,
 			name = 'confirm_create_feedback'),
 	path('further_comment', view.further_comment,
-			name = 'further_comment')
+			name = 'further_comment'),
+	path('confirm_submit', view.confirm_submit,
+			name = 'confirm_submit')
 ]
